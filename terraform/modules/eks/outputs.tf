882a5910c1479a494df6ee03d94106bbc84fc092
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-04 22:03:25 +0530

    Add terraform/modules/eks/outputs.tf

---
+++ terraform/modules/eks/outputs.tf
@@ -0,0 +1,69 @@
+output "cluster_id" {
+  description = "The name/id of the EKS cluster"
+  value       = aws_eks_cluster.main.id
+}
+
+output "cluster_arn" {
+  description = "The Amazon Resource Name (ARN) of the cluster"
+  value       = aws_eks_cluster.main.arn
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for your Kubernetes API server"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_version" {
+  description = "The Kubernetes version for the cluster"
+  value       = aws_eks_cluster.main.version
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = aws_security_group.cluster.id
+}
+
+output "node_security_group_id" {
+  description = "Security group ID attached to the EKS node group"
+  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "cluster_iam_role_name" {
+  description = "IAM role name associated with EKS cluster"
+  value       = aws_iam_role.cluster.name
+}
+
+output "cluster_iam_role_arn" {
+  description = "IAM role ARN associated with EKS cluster"
+  value       = aws_iam_role.cluster.arn
+}
+
+output "node_group_iam_role_name" {
+  description = "IAM role name associated with EKS node group"
+  value       = aws_iam_role.node_group.name
+}
+
+output "node_group_iam_role_arn" {
+  description = "IAM role ARN associated with EKS node group"
+  value       = aws_iam_role.node_group.arn
+}
+
+output "eks_managed_node_groups" {
+  description = "Map of attribute maps for all EKS managed node groups created"
+  value       = aws_eks_node_group.node_groups
+}
+
+output "oidc_provider_arn" {
+  description = "The ARN of the OIDC Provider if enabled"
+  value       = aws_iam_openid_connect_provider.cluster.arn
+}
+
+output "cluster_primary_security_group_id" {
+  description = "The cluster primary security group ID created by EKS"
+  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+}
